1e8747dbb51535e8f98eda5a753e200a3627d62e
Marcin <markowitek@gmail.com>
2018-05-23 19:14:53 +0200

    initial commit

---
+++ captchaRecognition/src/main/java/com/cracow/si/captchaRecognition/CaptchaRecognitionApplication.java
@@ -0,0 +1,12 @@
+package com.cracow.si.captchaRecognition;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaptchaRecognitionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaptchaRecognitionApplication.class, args);
+	}
+}
